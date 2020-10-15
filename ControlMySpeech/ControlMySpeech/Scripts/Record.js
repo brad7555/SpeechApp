c5e3b1ca868e6b84e42ba4482fc125d24a63567c
brad christensen <bradchristensen755@gmail.com>
2020-10-15 11:17:02 -0700

    Finished deleting a file feature

---
+++ ControlMySpeech/ControlMySpeech/Scripts/Record.js
@@ -0,0 +1,31 @@
+﻿function DeleteFile(id) {
+    //Sending the ID to the console for testing 
+    console.log(id);
+
+    //Making the user confrim that they want the file deleted
+    if (confirm("Delete File?")) {
+        //Crafting the URL to contain the ID so it can be 
+        //sent back to the controller function
+        var URL = "/Record/DeleteFile?ID=" + id;
+        //Ajax call to controller, sending the ID
+        $.ajax({
+            type: 'POST',
+            dataType: 'json',
+            url: URL,
+            
+            success: function (response) {
+                location.reload(); 
+                console.log("Success")
+
+            },
+            error: errorOnAjax,
+            
+        });
+    }
+
+}
+//Function to send an error message back to the console
+function errorOnAjax() {
+    console.log("Error on Ajax Return"); 
+
+}
